084a4ef76957270efefef657fadd125cac12ef42
NullChips <tommyjhns@gmail.com>
2017-04-28 19:14:37 +0100

    Initial commit.

---
+++ me/nullchips/bukkitplace/BukkitPlace.java
@@ -0,0 +1,32 @@
+package me.nullchips.bukkitplace;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Copyright (c) NullChips 2017. All rights reserved.
+ * All code contained within this document is the
+ * sole property of NullChips. Distribution, reproduction,
+ * taking snippets or claiming any contents as your own will
+ * break the terms of the license, and void any agreements with
+ * you, the third party.
+ * Thanks.
+ */
+public class BukkitPlace extends JavaPlugin {
+
+    private static JavaPlugin instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+    }
+
+    @Override
+    public void onDisable() {
+        instance = null;
+    }
+
+    public static JavaPlugin getInstance() {
+        return instance;
+    }
+
+}
